1b523e4cff1bc8addb4530fd47138a1ca58aabb4
LaisZagati <laisviana97@gmail.com>
2025-10-06 10:58:26 -0400

    Phonetics rendering fixed

--- src/Results.js
+++ src/Results.js
@@ -5,18 +5,19 @@ import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
+    // Pick the first phonetic with audio if possible, otherwise the first one
+    const phonetic =
+      props.results.phonetics.find((p) => p.audio) ||
+      props.results.phonetics[0];
+
     return (
       <div className="Results">
         <section>
           <h2>{props.results.word}</h2>
-          {props.results.phonetics.map(function (phonetic, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            );
-          })}
+          {/* Render phonetic only once */}
+          {phonetic && <Phonetic phonetic={phonetic} />}
         </section>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
